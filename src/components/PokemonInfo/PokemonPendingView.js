4666efb56998513d7a990d5e81b77052260516c7
Evgeniy <evgnec@gmail.com>
2025-06-14 18:53:50 +0300

    upd

--- src/components/PokemonInfo/PokemonPendingView.js
+++ src/components/PokemonInfo/PokemonPendingView.js
@@ -3,8 +3,13 @@ import pendingImage from './pending.jpeg'
 import PokemonDataView from './PokemonDataView';
 
 const styles = {
-
-}
+    spinner: {
+      display: 'flex',
+      alignItems: 'center',
+      marginBottom: 10,
+      fontSize: 24,
+    },
+  };
 
 export default function PokemonPendingView({ name }) {
 const  pokemon = {
@@ -21,9 +26,7 @@ const  pokemon = {
 
     return (
 <div role="alert">
-<div 
-// style={styles.spiner}
->
+<div style={styles.spinner}>
     <ImSpinner size="32" className='icon-spin'/>
     Loading ...
 </div>
