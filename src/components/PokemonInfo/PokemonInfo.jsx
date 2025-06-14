93ed7262825ed1eed5441afc42fb624364a96ac8
Evgeniy <evgnec@gmail.com>
2025-06-14 17:24:12 +0300

    catch

--- src/components/PokemonInfo/PokemonInfo.jsx
+++ src/components/PokemonInfo/PokemonInfo.jsx
@@ -7,23 +7,41 @@ export class PokemonInfo extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.name !== this.props.name) {
+    const prevName = prevProps.name;
+    const nextName = this.props.name;
+    if (prevName !== nextName) {
       this.setState({ loading: true });
-      fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.name}`)
-        .then(res => res.json())
+      fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+        // .then(res => res.json())
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(
+            new Error(`Not found pokemon with name ${nextName}`)
+          );
+        })
         .then(pokemon => this.setState({ pokemon }))
+        .catch(error => this.setState({ error }))
         .finally(() => this.setState({ loading: false }));
     }
   }
 
   render() {
-    const { loading, pokemon } = this.state;
+    const { loading, pokemon, error } = this.state;
     const { name } = this.props;
     return (
       <div>
+        {error && <h1>{error.message}</h1>}
         {loading && <div>Loading</div>}
         {!name && <div>Input name</div>}
         {pokemon && <div>{pokemon.name}</div>}
+        {pokemon && (
+          <img
+            src={pokemon.sprites.other[`official-artwork`].front_default}
+            alt={pokemon.name}
+          />
+        )}
       </div>
     );
   }
